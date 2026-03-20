8e3be7ef3592f58e33bd5b570e00cf6ad19b5c49
Ahmad <ahmadkhadim238@gmail.com>
2026-03-20 10:54:39 +0500

    new update

--- index.js
+++ index.js
@@ -19,27 +19,142 @@
 //                        Get API data from mongodb with mongoose
 //                  ============ ====================== ===============
 
-import mongoose from "mongoose";
-import express from 'express';
-import studentModel from "./model/studentModel.js";
+// import mongoose from "mongoose";
+// import express from 'express';
+// import studentModel from "./model/studentModel.js";
 
-await mongoose.connect("mongodb://localhost:27017/firstdb").then(() => {
-    console.log("__________connected");
+// await mongoose.connect("mongodb://localhost:27017/firstdb").then(() => {
+//     console.log("__________connected");
 
-})
+// })
 
-const app = express();
+// const app = express();
 
-app.get('/', async (req, resp) => {
+// app.get('/', async (req, resp) => {
 
-    const studentData = await studentModel.find();
+//     const studentData = await studentModel.find();
 
-    resp.send(studentData)
-})
-app.listen(3200)
+//     resp.send(studentData)
+// })
+// app.listen(3200)
+
 
 
 
 //                  ============ ====================== ===============
 //                        Make post API in Node with mongoose
 //                  ============ ====================== ===============
+// import mongoose from "mongoose";
+// import express from 'express'
+// import studentModel from "./model/studentModel.js";
+
+// const app = express()
+
+// app.use(express.json());
+
+// await mongoose.connect('mongodb://localhost:27017/firstdb').then(() => {
+//     console.log("__________connect_____");
+
+// })
+
+// app.get('/', async (req, resp) => {
+//     const studentData = await studentModel.find()
+//     resp.send(studentData)
+// })
+
+// app.post('/save', async (req, resp) => {
+//     console.log(req.body);
+
+//     const { name, email, age } = req.body;
+//     if (!req.body || !name | !age | !email) {
+//         resp.send({
+//             message: "Data not Stored",
+//             success: false,
+//             storedInfo: null
+//         })
+//         return false;
+//     }
+
+//     const studentData = await studentModel.create(req.body)
+
+//     resp.send({
+//         message: "Data  Stored",
+//         success: true,
+//         storedInfo: studentData
+//     })
+// })
+
+// app.listen(3200)
+
+
+
+
+
+//                  ============ ====================== ===============
+//                        Make post API in Node with mongoose
+//                  ============ ====================== ===============
+
+import mongoose from "mongoose";
+import express from 'express'
+import studentModel from "./model/studentModel.js";
+
+const app = express()
+
+app.use(express.json());
+
+await mongoose.connect('mongodb://localhost:27017/firstdb').then(() => {
+    console.log("__________connect_____");
+
+})
+
+app.get('/', async (req, resp) => {
+    const studentData = await studentModel.find()
+    resp.send(studentData)
+})
+
+app.post('/save', async (req, resp) => {
+    console.log(req.body);
+    const { name, email, age } = req.body;
+    if (!req.body || !name | !age | !email) {
+        resp.send({
+            message: "Data not Stored",
+            success: false,
+            storedInfo: null
+        })
+        return false;
+    }
+    const studentData = await studentModel.create(req.body)
+    resp.send({
+        message: "Data  Stored",
+        success: true,
+        storedInfo: studentData
+    })
+})
+
+app.put('/update/:id', async (req, resp) => {
+    const id = req.params.id
+    console.log(req.body, id);
+
+    const studentData = await studentModel.findByIdAndUpdate(id, {
+        ...req.body
+    })
+
+    resp.send({
+        message: "Data updated",
+        success: true,
+        info: studentData
+    })
+})
+app.delete('/delete/:id', async (req, resp) => {
+    const id = req.params.id
+
+    const studentData = await studentModel.findByIdAndDelete(id)
+
+    resp.send({
+        message: "Data updated",
+        success: true,
+        info: studentData
+    })
+})
+
+app.listen(3200)
